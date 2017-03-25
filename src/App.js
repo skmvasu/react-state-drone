5fb5cc80afb75198390efc32a294527963078458
Vasu K <skmvasu@gmail.com>
2017-03-25 17:08:28 +0530

    Updates readme with instructions and boundaries

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {
@@ -96,7 +97,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Scripbox Drone task</h2>
+          <h2>Simple Drone State Manger</h2>
         </div>
         <div className="App-intro">
           <Drone
